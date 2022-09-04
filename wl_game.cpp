6a134d342c14554266d89a712dbd96ac16f4ec45
vbt1 <vberthelot@free.fr>
2022-09-04 23:45:21 +0200

    force commit

--- wl_game.cpp
+++ wl_game.cpp
@@ -1,5 +1,5 @@
 // WL_GAME.C
-#define USE_SPRITES 1
+//#define USE_SPRITES 1
 #include <math.h>
 #include "wl_def.h"
 //#include <SDL_mixer.h>
@@ -503,8 +503,6 @@ void SetupGameLevel (void)
     word *map;
     word tile;
 
-//	slIntFunction(VblIn) ; // enlevé et mis au boot
-
     //if (!loadedgame)
     {
         gamestate.TimeCount
@@ -545,6 +543,11 @@ void SetupGameLevel (void)
 //
     memset (tilemap,0,sizeof(tilemap));
     memset (actorat,0,sizeof(actorat));
+	memset(objactor, 0, sizeof(objactor));	
+
+	InitActorList();	/* start spawning things with a clean slate */
+	InitDoorList();
+	InitStaticList();
 	
     map = mapsegs[0];
     for (y=0;y<mapheight;y++)
@@ -557,7 +560,6 @@ void SetupGameLevel (void)
                 // solid wall
                 tilemap[x][y] = (byte) tile;
 #ifdef EMBEDDED
-//				actorat[x][y] = tile;
 				set_wall_at(x, y, tile);
 #else
                 actorat[x][y] = (objtype *)(uintptr_t) tile;
@@ -567,17 +569,15 @@ void SetupGameLevel (void)
             {
                 // area floor
                 tilemap[x][y] = 0;
+#ifndef EMBEDDED				
                 actorat[x][y] = 0;
+#endif				
             }
         }
     }
 //
 // spawn doors
 //
-    InitActorList ();                       // start spawning things with a clean slate
-    InitDoorList ();
-    InitStaticList ();
-
     map = mapsegs[0];
     for (y=0;y<mapheight;y++)
     {
@@ -612,7 +612,7 @@ void SetupGameLevel (void)
     }
 // vbt : on recharge la vram
 #ifdef USE_SPRITES
-	memset(texture_list,0xff,SPR_NULLSPRITE);
+	memset(texture_list,0x00,SPR_NULLSPRITE);
 	position_vram=(SATURN_WIDTH+64)*32;
 
 	if(viewheight == screenHeight)
